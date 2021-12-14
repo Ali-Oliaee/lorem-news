dfc4ad1fc2a39f971419f9392b761b110a52ad9e
Ali Oliaee <alioliaee31@gmail.com>
2021-12-14 10:13:18 +0330

    ✨ add eslint and antd

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {Button} from 'antd'
+
+const App = function () {
+  return (
+    <div className="App">
+      <Button>ckdsn</Button>
+      <h3>cdscs</h3>
+    </div>
+  )
+}
+
+export default App
