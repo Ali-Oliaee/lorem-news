a57e5dd0e1d8b88ed5d5d0d50de28e7f20335e08
Ali Oliaee <alioliaee31@gmail.com>
2021-12-14 11:53:02 +0330

    💄 customize login form

--- src/components/login-wrapper/header/header.tsx
+++ src/components/login-wrapper/header/header.tsx
@@ -3,21 +3,24 @@ import {Button, Select} from 'antd'
 import './style.scss'
 
 function Header() {
-    const {Option} = Select
 
     return (
         <div className="login-wrapper-header">
             <div className="logo">
                 <h4>Lorem News</h4>
             </div>
-            <div className="language-container" >
-            
-                <Select defaultValue="EN" className="language-select">
-                    <Option value="en">EN</Option>
-                    <Option value="fa">فا</Option>
+            <div className="language-container">
+            <span>Language: </span>
+                <Select defaultValue="EN" className="language-select"  bordered={false}
+                showArrow={false}
+                dropdownMatchSelectWidth={false}
+                style={{width: 80}}
+                >
+                    <Select.Option value="en" key="en">EN</Select.Option>
+                    <Select.Option value="fa" key="fa">fa</Select.Option>
                 </Select>
-                
             </div>
+                
         </div>
     )
 }
--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -6,17 +6,22 @@ import './style.scss'
 function LoginPage() {
     return (
        <LoginWrapper>
-           <Form className="login-form">
-            <Form.Item className="input-container">
-                <Input placeholder="email"/>
-            </Form.Item>
-            <Form.Item className="input-container">
-                <Input.Password placeholder="password"/>
-            </Form.Item>
-            <Button htmlType="submit" className="submit-button" size="large">
-                Login
-            </Button>
-           </Form>
+           <section className="login-form-container">
+               <h5>Login Form</h5>
+                <Form className="login-form">
+                <Form.Item className="input-container">
+                    <Input placeholder="email"/>
+                </Form.Item>
+                <Form.Item className="input-container">
+                    <Input.Password placeholder="password"/>
+                </Form.Item>
+                <a href="#">forot your passwor?</a>
+                <Button htmlType="submit" className="submit-button" size="large">
+                    Login
+                </Button>
+                </Form>
+                <a href="./signup" className="signup-link">signup</a>
+           </section>
        </LoginWrapper>
     )
 }
