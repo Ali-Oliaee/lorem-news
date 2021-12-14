2ce6c6747b6188a468ee605d29fc803606bc7db4
Ali Oliaee <alioliaee31@gmail.com>
2021-12-14 12:05:33 +0330

    💄 add signup page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import {Button} from 'antd'
 import { LoginWrapper } from "./components/login-wrapper"
 import { LoginPage } from "./pages/login-page"
+import { SignupPage } from "./pages/signup"
 
 const App = function () {
   return (
     <div className="App">
-      <LoginPage/>
+      <SignupPage/>
     </div>
 
   )
