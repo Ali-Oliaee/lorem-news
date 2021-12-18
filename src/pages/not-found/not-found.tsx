30c390964ab76086781dcfcf17ae6294c980a28a
Ali Oliaee <alioliaee31@gmail.com>
2021-12-18 19:19:08 +0330

    💄 create page structure

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 import './style.scss'
 
 const NotFoundPage = () => {
+    const navigate = useNavigate()
+    const redirectUser = () => {
+        localStorage.getItem('email') ?
+        navigate('/profile') :
+        navigate('/auth/login')
+    }
+
     return(
-        <h4>page created</h4>
+        <div className="not-found-page">
+            <h1>Oops!</h1>
+            <h3>404 - PAGE NOT FOUND</h3>
+            <h5>The page is missing</h5>
+            <Button onClick={redirectUser}>Home page</Button>
+        </div>
     )
 }
 
